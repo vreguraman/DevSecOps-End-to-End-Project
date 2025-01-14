f6f429dc2dbba12dc95e864dc1e6c598c54b895a
Nuthan <Nuthan.gatla@gmail.com>
2025-01-14 21:28:17 +0530

    Devsecops

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ provider "vault" {}
 # Configure the AWS provider
 provider "aws" {
   region      = "us-east-1"
-  access_key  = data.vault_generic_secret.aws_credentials.data.access_key
-  secret_key  = data.vault_generic_secret.aws_credentials.data.secret_key
+  access_key  = "data.vault_generic_secret.aws_credentials.data.access_key"
+  secret_key  = "data.vault_generic_secret.aws_credentials.data.secret_key"
   
 }
 
