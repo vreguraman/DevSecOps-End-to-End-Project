37bd9e31326f23785fdf514f74b5a866847cf880
Nuthan <Nuthan.gatla@gmail.com>
2025-02-18 03:37:33 +0530

    Devsecops

--- src/test/jest.config.js
+++ src/test/jest.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
-  collectCoverage: true, // Enables code coverage report
-  coverageDirectory: "./coverage", // Specifies output directory for coverage reports
-  coverageReporters: ["json", "lcov", "text", "clover"], // Generates multiple coverage formats
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Exclude unnecessary folders
   reporters: [
-    "default",
-    ["jest-junit", { outputDirectory: "./test-results", outputName: "results.xml" }]
-  ],
-  verbose: true, // Enables detailed test execution logs
+    'default',
+    ['jest-junit', { outputDirectory: './test-results', outputName: 'results.xml' }]
+  ]
 };
-
-  
